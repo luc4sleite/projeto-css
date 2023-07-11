c5db4feac36f0eeefce4a64eae8973d3a1f17452
luc4sleite <lucahleite@gmail.com>
2023-07-10 22:34:20 -0300

    feat: sobre mim e minha jornada

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var body = document.querySelector('body');
-var themeButton = document.querySelector('.toggle');
+var body = document.querySelector("body");
+var themeButton = document.getElementById("theme-button");
 
-themeButton.addEventListener('click', function() {
-    if(body.classList.contains('dark')) {
-        body.classList.remove('dark');
-        body.classList.add('light');
-        themeButton.innerHTML = 'Modo Escuro';
+themeButton.addEventListener("click", function () {
+    if (body.classList.contains("dark")) {
+        body.classList.remove("dark");
+        body.classList.add("light");
+        themeButton.innerHTML = `
+            <span class="material-symbols-outlined">
+                dark_mode
+            </span>`;
     } else {
-        body.classList.remove('light');
-        body.classList.add('dark');
-        themeButton.innerHTML = 'Modo Claro';
+        body.classList.remove("light");
+        body.classList.add("dark");
+        themeButton.innerHTML = `
+            <span class="material-symbols-outlined"> 
+                light_mode 
+            </span>`;
     }
 });
